da57e15838b7bd57ca60f380a66f91c3842ac6bd
Vilson Borges <vilsonborges98@gmail.com>
2020-07-02 13:05:06 -0300

    ação de adcionar e remover tecnologias

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -21,12 +21,19 @@ class TechList extends Component{
             newTech: ''
         });
     }
-
+    handleDelete = (teck) => {
+        this.setState({ techs: this.state.techs.filter( t => t !== teck)});
+    }
     render(){
         return (
             <form onSubmit={this.handleSubmit}>
                 <ul>
-                    {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+                    {this.state.techs.map(tech => (
+                        <li key={tech}>
+                            {tech}
+                            <button onClick={() => this.handleDelete(tech)} type="button" >Remover</button>
+                        </li>
+                    ))}
                 </ul>
                 <input 
                     type="text" 
